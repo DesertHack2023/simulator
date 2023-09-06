8d69be3e85f33bba64e23b4c4eb64f852d747a09
clodman84 <cshourin@gmail.com>
2023-09-07 01:07:22 +0530

    fixed edge snapping (hopeless typo)

--- GUI/frame_renderer.py
+++ GUI/frame_renderer.py
@@ -12,8 +12,7 @@ SNAP_DISTANCE = 25
 
 
 def distance(p1: tuple[float, float], p2: tuple[float, float]):
-    vector = [p2[0] - p1[0], p2[1] - p1[1]]
-    return sqrt(sum(map(lambda x: x * x, vector)))
+    return sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
 
 
 @dataclass
@@ -21,27 +20,29 @@ class Edge:
     p1: tuple[float, float]
     p2: tuple[float, float]
     edge_type: int = 0
+    length: float = 0
     unit_vector: tuple[float, float] = field(init=False)
 
     def __post_init__(self):
         vector = [self.p2[0] - self.p1[0], self.p2[1] - self.p1[1]]
-        magnitude = sqrt(sum(map(lambda x: x * x, vector)))
+        magnitude = sqrt(vector[0] ** 2 + vector[1] ** 2)
         self.unit_vector = tuple(i / magnitude for i in vector)
+        self.length = distance(self.p1, self.p2)
 
     def draw(self, parent):
         dpg.draw_line(
             self.p1, self.p2, color=COLOUR, thickness=THICKNESS, parent=parent
         )
-        logger.debug("Drew line")
+        # logger.debug("Drew line")
 
     def foot_of_the_perpendicular(self, point: tuple[float, float]):
         dot = (point[0] - self.p1[0]) * self.unit_vector[0] + (
             point[1] - self.p1[1]
         ) * self.unit_vector[1]
-        if dot >= 0:
+        if dot >= 0 and dot < self.length:
             return (
                 self.p1[0] + dot * self.unit_vector[0],
-                self.p2[1] + dot * self.unit_vector[1],
+                self.p1[1] + dot * self.unit_vector[1],
             )
 
 
@@ -68,14 +69,14 @@ class Frame:
     def _suggestion(self, point: tuple[float, float]):
         candidates = []
         for edge in self.edges:
-            logger.debug(f"Checking {edge}")
+            # logger.debug(f"Checking {edge}")
             foot = edge.foot_of_the_perpendicular(point)
             if not foot:
                 continue
             d = distance(foot, point)
-            logger.debug(f"Perpendicular distance {d}")
+            # logger.debug(f"Perpendicular distance {d}")
             if d < SNAP_DISTANCE:
-                logger.debug("Close Enough")
+                # dpg.draw_circle(edge.p1, radius=2, color=(0, 0, 255, 255), parent=self.drawlist)
                 candidates.append((foot, d))
         if candidates:
             return min(candidates, key=lambda x: x[1])[0]
@@ -92,7 +93,8 @@ class Frame:
             suggestion = self._suggestion((new_x, new_y))
             if suggestion:
                 new_x, new_y = suggestion
-                logger.debug(f"Suggested Point {(new_x, new_y)}")
+                # logger.debug(f"Suggested Point {(new_x, new_y)}")
+                # dpg.draw_circle((new_x, new_y), radius=2, color=(255, 0, 0, 255), parent=self.drawlist)
             if line:
                 dpg.configure_item(line, p2=(new_x, new_y))
             else:
@@ -106,4 +108,4 @@ class Frame:
         if new_x != x or new_y != y:
             edge = Edge((x, y), (new_x, new_y))
             self.edges.append(edge)
-        logger.debug(self.edges)
+        # logger.debug(self.edges)
