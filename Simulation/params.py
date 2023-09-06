d84ab659e5c45cbc5426d48dd9dfc5e2d9c19eb7
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2023-09-06 14:08:09 +0530

    update params

--- Simulation/params.py
+++ Simulation/params.py
@@ -1,1 +1,21 @@
+from dataclasses import dataclass
+from math import log
 
+
+@dataclass
+class Params:
+    # Basic model related parameters
+    WIDTH = 100
+    HEIGHT = 100
+    POPULATION_SIZE = 2500
+    POPULATION_DEMOGRAPHICS = [0.35, 0.8, 0.95, 1]
+    SIMULATION_LENGTH = 180
+    MAX_MOVEMENT = 0.05
+    TRAVEL_RATE = 0.1
+    RANDOM_SEED = 0
+
+    # Contact radius
+    # The contact radius is a function of the population density
+    # CONTACT_RADIUS = 3 / POPULATION_DENSITY
+    CONTACT_RADIUS = 0.5
+    CONTACT_RADIUS_SQUARED = CONTACT_RADIUS**2
